9c38ce7e148deeb99dae20dd920fd6b30c7bed43
Monisha Ravikumar <monisha.r@cruxule.com>
2024-07-10 11:47:42 +0530

    Title: voicescribe-production
    Author: Monisha

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -9,14 +9,10 @@
 import 'package:audio_session/audio_session_web.dart';
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:file_picker/_internal/file_picker_web.dart';
-import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_storage_web/firebase_storage_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
-import 'package:geolocator_web/geolocator_web.dart';
-import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 import 'package:just_audio_web/just_audio_web.dart';
-import 'package:package_info_plus/src/package_info_plus_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
@@ -24,13 +20,9 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   AudioSessionWeb.registerWith(registrar);
   FirebaseFirestoreWeb.registerWith(registrar);
   FilePickerWeb.registerWith(registrar);
-  FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   FirebaseStorageWeb.registerWith(registrar);
   FluttertoastWebPlugin.registerWith(registrar);
-  GeolocatorPlugin.registerWith(registrar);
-  GoogleMapsPlugin.registerWith(registrar);
   JustAudioPlugin.registerWith(registrar);
-  PackageInfoPlusWebPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
